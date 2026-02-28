fa0e0c5cd172d88dab4fa234ec68f4b0e485fff7
Ofikur R. <ofikurr@gmail.com>
2026-02-28 15:14:17 +0700

    fix: type error on ongoing-anime id parameter

--- src/app/api/ongoing-anime/[id]/route.ts
+++ src/app/api/ongoing-anime/[id]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse, NextRequest } from "next/server"
-import ongoingAnime from "@/utils/ongoingAnime";
+import ongoingAnime from "@/utils/ongoingAnime"
 
-export async function GET(request: NextRequest, props: { params: Promise<{ id: number }> }) {
+export async function GET(request: NextRequest, props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
-  const data = await ongoingAnime(params.id)
+  const data = await ongoingAnime(Number(params.id))
   return NextResponse.json({ data: data }, { status: 200 })
 }
