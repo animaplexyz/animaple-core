cfd0e2538d47f816b037250b87b8afdd1b0e11cb
Ofikur R. <ofikurr@gmail.com>
2026-02-28 14:41:37 +0700

    fix: type error on episode route parameter

--- src/app/api/anime/[slug]/episodes/[episode]/route.ts
+++ src/app/api/anime/[slug]/episodes/[episode]/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse, NextRequest } from "next/server"
 import episode from "@/utils/episode";
 
-export async function GET(request: NextRequest, props: { params: Promise<{ anime_slug: string; episode: number }> }) {
+export async function GET(request: NextRequest, props: { params: Promise<{ slug: string; episode: string }> }) {
   const params = await props.params;
   const urlParts = request.url.split("/")
   const animeSlug = urlParts[5]
-  const data = await episode({ animeSlug: animeSlug, episodeNumber: params.episode })
+  const data = await episode({ animeSlug: animeSlug, episodeNumber: Number(params.episode) })
   return NextResponse.json({ data: data }, { status: 200 })
 }
