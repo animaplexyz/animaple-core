c36521fa8aa520f30611afcbd8cd880e70aba704
Ofikur R. <ofikurr@gmail.com>
2026-02-28 15:42:58 +0700

    feat: add redirect from root to api page in next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/api',
+        permanent: true,
+      },
+    ]
+  },
   async headers() {
     return [
       {
